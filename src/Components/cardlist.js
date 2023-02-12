cfd6026ffef3189ac708d1414df34847577bdd93
BiliqisO <biliqisonikoyi@gmail.com>
2023-02-12 12:20:53 +0100

    first commit

---
+++ src/Components/cardlist.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "./card";
+
+const Cardlist = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((robot) => {
+        return (
+          <Card
+            key={robot.id}
+            id={robot.id}
+            name={robot.name}
+            email={robot.email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+export default Cardlist;
